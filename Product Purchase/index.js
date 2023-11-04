838951fd481ac30d80583a970422c2c3a008caab
BigLens <adedejibioku@gmail.com>
2023-11-04 18:54:35 +0100

    js updated

--- Product Purchase/index.js
+++ Product Purchase/index.js
@@ -1,11 +1,20 @@
+ const buttonFirst = document.getElementById("btn")
+ const textsRender = document.getElementById("text")
+ let paraGraph = "Sorry, you have insufficient fund!"
+ buttonFirst.addEventListener("click", function(){
+     textsRender.innerHTML += paraGraph
+ }) 
+const btnSecond = document.getElementById("second-btn")
+let another = document.getElementById("btntwo")
 
+let randomPayment = ["Enter Your Credit Card", "Pay With Bank", "Pay With Coupon", "Buy Now, Pay Later"];
 
-
-
-
-
-
-
+btnSecond.addEventListener("click", function(){
+    for (i = 0; i < randomPayment.length; i++){
+        randomPayment[i] += Math.floor(Math.random())
+        another.innerHTML += randomPayment[i] + " "
+    }
+})
 
 
 
